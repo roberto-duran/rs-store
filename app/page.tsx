1f7327082f048df3f3867d0b7830f1830e3ae151
Roberto Duran <shaggyrd+github@gmail.com>
2023-02-24 08:26:10 -0500

    remove redux in favor of the new next 13.2.0 cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 'use client';
-import {Provider} from "react-redux";
-import store from "../store";
-import {PersistGate} from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
-
-let persistor = persistStore(store);
 
 export default function Home() {
     return (
         <main className="text-3xl font-bold underline">
-            <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                    <h1>hola</h1>
-                </PersistGate>
-            </Provider>
+            <h1>hello</h1>
         </main>
     );
 }
